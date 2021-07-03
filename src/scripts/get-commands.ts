332b0119216b968eafdaf89e42a4f08880c5bd29
Matthew Elphick <matt.a.elphy@gmail.com>
2021-07-03 12:32:04 +0100

    chore: Add script to get commands

---
+++ src/scripts/get-commands.ts
@@ -0,0 +1,15 @@
+import fetch from 'isomorphic-fetch'
+
+const APP_ID = '850402324421279774'
+
+;(async () => {
+  const res = await fetch(`https://discord.com/api/v8/applications/${APP_ID}/commands`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bot ${process.env.BOT_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  })
+
+  console.info('[result]', JSON.stringify(await res.json(), undefined, 2))
+})().catch((e) => console.error(e))
