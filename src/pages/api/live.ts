3dba147f6ac106b8dc2db0573060a967aee475a4
Matt Elphick <matt.a.elphy@gmail.com>
2021-12-14 15:47:03 +0000

    feat: More info on live channels, fix yt liveness

--- src/pages/api/live.ts
+++ src/pages/api/live.ts
@@ -4,9 +4,48 @@ import { cors } from '../../util'
 const TWITCH_TOKEN = process.env.TWITCH_TOKEN
 
 async function getYoutubeLive(channel: string) {
-  const channelLink = `https://www.youtube.com/c/${channel}`
+  const channelLink = `https://www.youtube.com/c/${encodeURIComponent(channel)}`
   const result = await fetch(channelLink).then((r) => r.text())
-  return { channel, isLive: result.includes('live streams'), type: 'youtube', link: channelLink }
+  const data = result.match(/var ytInitialData = (?<data>{[\s\S]+?)<\/script>/)
+  const obj = data?.groups?.data?.slice(0, -1)
+  let resultData: any = null
+  try {
+    let liveItem: any = null
+    JSON.parse(obj || '{}').contents?.twoColumnBrowseResultsRenderer?.tabs?.some((t) =>
+      t.tabRenderer?.content?.sectionListRenderer?.contents?.some((c1) =>
+        c1.itemSectionRenderer?.contents?.some(
+          (c2) =>
+            c2.channelFeaturedContentRenderer &&
+            c2.channelFeaturedContentRenderer.items.some((i) => {
+              const result = i.videoRenderer.thumbnailOverlays.some(
+                (to) => to.thumbnailOverlayTimeStatusRenderer.style === 'LIVE'
+              )
+              if (result) liveItem = i.videoRenderer
+              return result
+            })
+        )
+      )
+    )
+    const isLive = !!liveItem
+    if (isLive) {
+      const title = liveItem.title.runs.map((r) => r.text).join('')
+      const viewCount = liveItem.viewCountText.runs.map((r) => r.text).join('')
+      const thumbnail = liveItem.thumbnail.thumbnails[0].url
+      const description = liveItem.descriptionSnippet.runs.map((r) => r.text).join('')
+      const url = `https://youtube.com${liveItem.navigationEndpoint.commandMetadata.webCommandMetadata.url}`
+      resultData = { title, viewCount, thumbnail, description, channel, url }
+    }
+  } catch (e) {
+    // Do nothing
+    console.info('[e]', channel, e)
+  }
+  return {
+    channel,
+    isLive: !!resultData,
+    type: 'youtube',
+    link: channelLink,
+    data: resultData,
+  }
 }
 
 async function getTwitchLive(users: string[]) {
@@ -22,23 +61,41 @@ async function getTwitchLive(users: string[]) {
     console.error('Got:', result)
     return []
   }
-  return users.map((u) => ({
-    channel: u,
-    type: 'twitch',
-    isLive: result.some((r) => r.user_login === u && r.type === 'live'),
-    link: `https://twitch.tv/${u}`,
-  }))
+  return users.map((u) => {
+    const data = result.find((r) => r.user_login === u && r.type === 'live')
+    let resultData: any = null
+    if (data) {
+      resultData = {
+        title: data.title,
+        viewCount: `${data.viewer_count} watching`,
+        thumbnail: data.thumbnail_url.replace('{width}', 168).replace('{height}', 94),
+        description: '',
+        channel: u,
+        url: `https://twitch.tv/${u}`,
+        game: data.game_name,
+      }
+    }
+    return {
+      channel: u,
+      type: 'twitch',
+      isLive: !!data,
+      link: `https://twitch.tv/${u}`,
+      data: resultData,
+    }
+  })
 }
 
 const handler: NextApiHandler = async (req, res) => {
   const youtubeChannels = (req.query.youtube || '')
     .toString()
     .split(',')
-    .map((i) => i.trim())
+    .map((i) => decodeURIComponent(i.trim()))
+    .filter(Boolean)
   const twitchUsers = (req.query.twitch || '')
     .toString()
     .split(',')
-    .map((i) => i.trim())
+    .map((i) => decodeURIComponent(i.trim()))
+    .filter(Boolean)
   const [youtube, twitch] = await Promise.all([
     Promise.all(youtubeChannels.map(getYoutubeLive)),
     getTwitchLive(twitchUsers),
