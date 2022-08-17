b182dc8849b301e2c1ffbba99dbfdfe49d3de081
Matt Elphick <matt.a.elphy@gmail.com>
2022-08-17 22:05:23 +0100

    feat: Add twitch chatters page

---
+++ src/pages/api/twitch-chatters.ts
@@ -0,0 +1,10 @@
+import { NextApiHandler } from 'next'
+import { cors } from '../../util'
+
+const handler: NextApiHandler = async (req, res) => {
+  const channel = req.query.channel.toString()
+  const chatters = await fetch(`http://tmi.twitch.tv/group/user/${channel}/chatters`).then((res) => res.json())
+  res.json(chatters)
+}
+
+export default cors(handler)
