17607cae8054af256c0682231a97e63adfe3248b
Matt Elphick <matt.a.elphy@gmail.com>
2023-12-19 09:12:37 +0000

    feat: Try connecting to mongo

--- src/interactions.ts
+++ src/interactions.ts
@@ -1,6 +1,33 @@
+import { MongoClient } from 'mongodb'
+import { format } from 'date-fns/format'
+
+interface StateItGuess {
+  day: string
+  stateId: number
+  answer: number
+  guesses: {
+    [guesser: string]: string
+  }
+}
+
 export default {
   'stateit-guess': async (body) => {
-    console.info('[interaction][stateit-guess]', { member: body.member, values: body.data.values })
+    const gameDay = format(new Date(body.message.timestamp), 'dd/MM/yy')
+    console.info('[interaction][stateit-guess]', { gameDay, member: body.member, values: body.data.values })
+    let client: MongoClient | null = null
+    try {
+      client = new MongoClient(process.env.MONGODB_URI!)
+      const database = client.db('discord')
+      const stateItGuesses = database.collection<StateItGuess>('state-it-guesses')
+      await stateItGuesses.updateOne(
+        { day: gameDay },
+        { $set: { [`guesses.${body?.member?.user?.id}`]: `${body.data.values[0]}` } }
+      )
+    } catch (e) {
+      console.error('[interaction][stateit-guess][mongo][error]', e)
+    } finally {
+      if (client) await client.close()
+    }
     return {
       type: 4,
       data: {
