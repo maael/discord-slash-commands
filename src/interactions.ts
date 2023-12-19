cb0d5a3a47aa5a315ae62f8c11ef224207430e41
Matt Elphick <matt.a.elphy@gmail.com>
2023-12-19 08:44:23 +0000

    fix: Add more logging/debugging for interaction issue

--- src/interactions.ts
+++ src/interactions.ts
@@ -1,9 +1,12 @@
 export default {
   'stateit-guess': async (body) => {
+    console.info('[interaction][stateit-guess]', { member: body.member, values: body.data.values })
     return {
       type: 4,
       data: {
-        content: `<@${body.member.user.id}> guessed <@${body.data.values[0]}>!`,
+        content: `${body?.member?.user?.id ? `<@${body?.member?.user?.id}>` : 'You'} guessed <@${
+          body.data.values[0]
+        }>!`,
       },
     }
   },
