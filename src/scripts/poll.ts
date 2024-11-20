fbc193b994d63d93f48578fd5a1a01ec834e9387
Matt Elphick <matt.a.elphy@gmail.com>
2024-11-20 20:56:13 +0000

    fix: Update script to make a poll

--- src/scripts/poll.ts
+++ src/scripts/poll.ts
@@ -10,19 +10,11 @@
  * DISCORD_TOKEN - token for the bot to post as
  */
 
+import { SelectMenuBuilder } from '@discordjs/builders'
 import { addDays } from 'date-fns/addDays'
 import { eachDayOfInterval } from 'date-fns/eachDayOfInterval'
 import { format } from 'date-fns/format'
-import {
-  Client,
-  Events,
-  GatewayIntentBits,
-  ActionRowBuilder,
-  EmbedBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  TextChannel,
-} from 'discord.js'
+import { Client, Events, GatewayIntentBits, ActionRowBuilder, EmbedBuilder, TextChannel } from 'discord.js'
 ;(async () => {
   console.info('[start]')
   await sendMessage()
@@ -47,12 +39,6 @@ async function sendMessage() {
   const client = await getClient()
   const channel = (await client.channels.fetch(process.env.DISCORD_DND_POLL_CHANNEL!)) as TextChannel
   const days = getDays()
-  const buttons = days.map((d) => {
-    return new ButtonBuilder()
-      .setCustomId(`dnd-poll--${format(d, 'yyyy-MM-dd')}`)
-      .setLabel(format(d, 'EEEE dd/MM'))
-      .setStyle(ButtonStyle.Primary)
-  })
   const embedopen = new EmbedBuilder()
     .setTitle(`Dates for ${format(days.at(0)!, 'EEEE dd/MM/yy')} to ${format(days.at(-1)!, 'EEEE dd/MM/yy')}`)
     .setAuthor({ name: 'Hobby Scheduler' })
@@ -67,12 +53,19 @@ async function sendMessage() {
       Voted: No one
       Waiting on: <@137678852628545539>`
     )
-  const rows = chunkArray(buttons, 5)
-    .map((chunk) => new ActionRowBuilder<ButtonBuilder>().addComponents(...chunk))
-    .slice(0, 5)
   const _message = await channel.send({
     embeds: [embedopen],
-    components: rows,
+    components: [
+      new ActionRowBuilder<SelectMenuBuilder>().addComponents(
+        new SelectMenuBuilder({
+          custom_id: 'dnd-poll',
+          placeholder: 'Select dates',
+          max_values: 14,
+          min_values: 0,
+          options: days.map((d) => ({ label: format(d, 'EEEE dd/MM'), value: format(d, 'yyyy-MM-dd') })),
+        })
+      ),
+    ],
   })
   client.destroy()
 }
