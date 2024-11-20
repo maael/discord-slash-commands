bc31162336196e2549d438e4d34086694b8948c9
Matt Elphick <matt.a.elphy@gmail.com>
2024-11-20 22:16:55 +0000

    feat: Make votes go to DB

--- src/interactions/dnd-poll.ts
+++ src/interactions/dnd-poll.ts
@@ -1,5 +1,6 @@
-import { eachDayOfInterval, parse, format, isEqual } from 'date-fns'
+import { parse, format, eachDayOfInterval, isEqual } from 'date-fns'
 import { EmbedBuilder } from 'discord.js'
+import { MongoClient, ReturnDocument } from 'mongodb'
 
 function getRangeFromTitle(title: string) {
   const [from, to] = title
@@ -7,47 +8,123 @@ function getRangeFromTitle(title: string) {
     .replace(/[a-zA-Z ]/g, '')
     .trim()
     .split('-')
+    .map((i) => parse(i, 'dd/MM/yy', new Date()))
   return { from, to }
 }
 
+function parseDate(str: string) {
+  return parse(str, 'yyyy-MM-dd', new Date())
+}
+
+function getDays(start, end) {
+  return eachDayOfInterval({ start, end })
+}
+
 export async function handleDndPoll(body) {
-  const messageTitle = body.message.embeds.at(0).title
-  const messageDateRange = getRangeFromTitle(messageTitle)
-  const messageDescription = body.message.embeds.at(0).description
-  console.info('dnd-poll', {
-    channel: body.message.channel_id,
-    message: body.message.id,
-    username: body.member.user.username,
-    userId: body.member.user.id,
-    messageTitle,
-    messageDateRange,
-    messageDescription,
-    values: body.data.values,
-  })
-  const selectedDates = body.data.values.map((v) => parse(v, 'yyyy-MM-dd', new Date()))
-  const days = eachDayOfInterval({
-    start: parse(messageDateRange.from, 'dd/MM/yy', new Date()),
-    end: parse(messageDateRange.to, 'dd/MM/yy', new Date()),
-  })
-  const example = new EmbedBuilder().setTitle(messageTitle).addFields(
-    days
-      .map((d) => {
-        const voted = selectedDates.some((s) => isEqual(d, s))
-        return {
-          name: `${format(d, 'dd/MM - EEEE')}`,
-          value: `${voted ? 1 : 0}/5 - ${voted ? ':orange_circle:' : ':red_circle:'}`,
-        }
-      })
-      .concat([
-        { name: 'Voted', value: `<@${body.member.user.id}>` },
-        { name: 'Waiting on', value: 'No one' },
-      ])
-  )
-  console.info('updating to', example.toJSON())
-  return {
-    type: 7,
-    data: {
-      embeds: [example.toJSON()],
-    },
+  try {
+    const messageTitle = body.message.embeds.at(0).title
+    const messageDateRange = getRangeFromTitle(messageTitle)
+    const messageDescription = body.message.embeds.at(0).description
+    console.info('dnd-poll', {
+      channel: body.message.channel_id,
+      message: body.message.id,
+      username: body.member.user.username,
+      userId: body.member.user.id,
+      messageTitle,
+      messageDateRange,
+      messageDescription,
+      values: body.data.values,
+    })
+    const result = await updateAndGetScheduling(
+      messageDateRange.from,
+      messageDateRange.to,
+      body.member.user.id,
+      body.data.values
+    )
+    if (!result) {
+      throw new Error('Missing schedule')
+    }
+    const voted = new Set(Object.values(result.dates).flat(1))
+    const example = new EmbedBuilder()
+      .setTitle(
+        `Dates for ${format(parseDate(result.from), 'EEEE dd/MM/yy')} to ${format(
+          parseDate(result.to),
+          'EEEE dd/MM/yy'
+        )}`
+      )
+      .setAuthor({ name: 'Hobby Scheduler' })
+      .addFields(
+        Object.entries(result.dates)
+          .map(([d, v]: any) => ({
+            name: `${format(d, 'dd/MM - EEEE')}`,
+            value: `${
+              v.length === 0
+                ? ':red_circle:'
+                : v.length === result.members.length
+                ? ':green_circle:'
+                : ':orange_circle:'
+            } - ${v.length}/${result.members.length}`,
+          }))
+          .concat([
+            {
+              name: 'Voted',
+              value: [...voted].map((m) => `<@${m}>`).join(', ') || 'No one',
+            },
+            {
+              name: 'Waiting on',
+              value:
+                result.members
+                  .filter((m) => !voted.has(m))
+                  .map((m) => `<@${m}>`)
+                  .join(', ') || 'No one',
+            },
+          ])
+      )
+    console.info('updating to', example.toJSON())
+    return {
+      type: 7,
+      data: {
+        embeds: [example.toJSON()],
+      },
+    }
+  } catch (e: unknown) {
+    return {
+      type: 4,
+      data: {
+        content: `An error occurred: ${(e as Error).message}`,
+      },
+    }
+  }
+}
+
+function toUpdateObject(keys: Date[], value: string | string[]) {
+  return keys.reduce((acc, k) => ({ ...acc, [`dates.${format(k, 'yyyy-MM-dd')}`]: value }), {})
+}
+
+async function updateAndGetScheduling(from, to, userId, values) {
+  try {
+    const client = new MongoClient(process.env.MONGODB_URI!)
+    const database = client.db('discord')
+    const scheduling = database.collection<any>('hobby-scheduling')
+    const allDays = getDays(from, to)
+    const votedFor = values.map(parseDate)
+    const notVotedFor = allDays.filter((d) => !votedFor.some((v) => isEqual(v, d)))
+    console.info('votes', { votedFor, notVotedFor })
+    const update = {
+      $addToSet: toUpdateObject(votedFor, userId),
+      $pullAll: toUpdateObject(notVotedFor, [userId]),
+    }
+    console.info('update', JSON.stringify({ update }))
+    const result = await scheduling.findOneAndUpdate(
+      { from: format(from, 'yyyy-MM-dd'), to: format(to, 'yyyy-MM-dd') },
+      update,
+      { upsert: true, returnDocument: ReturnDocument.AFTER }
+    )
+    console.info('result', JSON.stringify({ result }))
+    await client.close()
+    return result
+  } catch (e) {
+    console.error(e)
+    return null
   }
 }
--- src/scripts/poll.ts
+++ src/scripts/poll.ts
@@ -11,10 +11,12 @@
  */
 
 import { SelectMenuBuilder } from '@discordjs/builders'
+import { parse } from 'date-fns'
 import { addDays } from 'date-fns/addDays'
 import { eachDayOfInterval } from 'date-fns/eachDayOfInterval'
 import { format } from 'date-fns/format'
 import { Client, Events, GatewayIntentBits, ActionRowBuilder, EmbedBuilder, TextChannel } from 'discord.js'
+import { MongoClient } from 'mongodb'
 ;(async () => {
   console.info('[start]')
   await sendMessage()
@@ -26,19 +28,42 @@ function getDays() {
   return eachDayOfInterval({ start: current, end: addDays(current, 13) })
 }
 
+function parseDate(str: string) {
+  return parse(str, 'yyyy-MM-dd', new Date())
+}
+
 async function sendMessage() {
   const client = await getClient()
   const channel = (await client.channels.fetch(process.env.DISCORD_DND_POLL_CHANNEL!)) as TextChannel
   const days = getDays()
+  const result = await createDbRecord(days)
+  const voted = new Set(Object.values(result.dates).flat(1))
   const embedopen = new EmbedBuilder()
-    .setTitle(`Dates for ${format(days.at(0)!, 'EEEE dd/MM/yy')} to ${format(days.at(-1)!, 'EEEE dd/MM/yy')}`)
+    .setTitle(
+      `Dates for ${format(parseDate(result.from), 'EEEE dd/MM/yy')} to ${format(parseDate(result.to), 'EEEE dd/MM/yy')}`
+    )
     .setAuthor({ name: 'Hobby Scheduler' })
     .addFields(
-      days
-        .map((d) => ({ name: `${format(d, 'dd/MM - EEEE')}`, value: `0/5 - :red_circle:` }))
+      Object.entries(result.dates)
+        .map(([d, v]: any) => ({
+          name: `${format(d, 'dd/MM - EEEE')}`,
+          value: `${
+            v.length === 0 ? ':red_circle:' : v.length === result.members.length ? ':green_circle:' : ':orange_circle:'
+          } - ${v.length}/${result.members.length}`,
+        }))
         .concat([
-          { name: 'Voted', value: 'No one' },
-          { name: 'Waiting on', value: '<@137678852628545539>' },
+          {
+            name: 'Voted',
+            value: [...voted].map((m) => `<@${m}>`).join(', ') || 'No one',
+          },
+          {
+            name: 'Waiting on',
+            value:
+              result.members
+                .filter((m) => !voted.has(m))
+                .map((m) => `<@${m}>`)
+                .join(', ') || 'No one',
+          },
         ])
     )
   const _message = await channel.send({
@@ -55,7 +80,30 @@ async function sendMessage() {
       ),
     ],
   })
-  client.destroy()
+  console.info({ result })
+  await client.destroy()
+}
+
+async function createDbRecord(days: Date[]) {
+  const from = format(days.at(0)!, 'yyyy-MM-dd')
+  const to = format(days.at(-1)!, 'yyyy-MM-dd')
+  const client = new MongoClient(process.env.MONGODB_URI!)
+  const database = client.db('discord')
+  const scheduling = database.collection<any>('hobby-scheduling')
+  const result = await scheduling.findOneAndUpdate(
+    { from, to },
+    {
+      $set: {
+        from,
+        to,
+        dates: days.reduce((acc, d) => ({ ...acc, [format(d, 'yyyy-MM-dd')]: [] })),
+        members: ['137678852628545539', '185741150714331136', '217741486115127297'],
+      },
+    },
+    { upsert: true }
+  )
+  await client.close()
+  return result
 }
 
 async function getClient() {
